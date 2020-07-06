edca33d10f2262a91468eab76420c31e213c9715
YadelskyiDev <leCarreacute@gmail.com>
2020-07-06 16:34:54 +0300

    create AnswersList

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -4,7 +4,16 @@ import ActiveQuiz from '../../components/ActiveQuiz/ActiveQuiz'
 
 export default class Quiz extends Component {
     state = {
-        quiz: []
+        quiz: [
+            {
+                answers: [
+                    {text: 'question 1'},
+                    {text: 'question 2'},
+                    {text: 'question 3'},
+                    {text: 'question 4'},
+                ]
+            }
+        ]
     }
 
 
@@ -13,8 +22,9 @@ export default class Quiz extends Component {
             <div className={classes.Quiz}>
                 <div className={classes.QuizWrapper}>
                     <h1>Quiz</h1>
-
-                    <ActiveQuiz/>
+                    <ActiveQuiz
+                        answers={this.state.quiz[0].answers}
+                    />
                 </div>
 
             </div>
