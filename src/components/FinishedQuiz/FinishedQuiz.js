ae7aaa6130b9af603698b1fffee7794410b6eacf
YadelskyiDev <leCarreacute@gmail.com>
2020-07-16 17:16:00 +0300

    routing

--- src/components/FinishedQuiz/FinishedQuiz.js
+++ src/components/FinishedQuiz/FinishedQuiz.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import classes from './FinishedQuiz.module.css'
 import Button from '../UI/Button/Button'
+import {Link} from 'react-router-dom'
 
 const FinishedQuiz = props => {
     const successCount = Object.keys(props.results).reduce((total, key) => {
@@ -36,7 +37,9 @@ const FinishedQuiz = props => {
 
             <div>
                 <Button onClick={props.onRetry} type="primary">Repeat</Button>
-                <Button  type="success">To test list</Button>
+                <Link to='/'>
+                    <Button type="success">To test list</Button>
+                </Link>
             </div>  
         </div>
     )
