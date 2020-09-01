59b1f31d5d23f6538bd7c1f6e9e732c217f12125
YadelskyiDev <leCarreacute@gmail.com>
2020-09-01 11:39:20 +0300

    autoLogin

--- src/components/Navigation/Drawer/Drawer.js
+++ src/components/Navigation/Drawer/Drawer.js
@@ -3,11 +3,6 @@ import classes from './Drawer.module.css'
 import {NavLink} from 'react-router-dom'
 import Backdrop from '../../UI/Backdrop/Backdrop'
 
-const links = [
-    {to: '/', label:'Список', exact: true},
-    {to: '/auth', label:'Авторизація', exact: false},
-    {to: '/quiz-creator', label:'Створити тест', exact: false},
-]
 
 class Drawer extends Component {
 
@@ -15,7 +10,7 @@ class Drawer extends Component {
         this.props.onClose()
     }
 
-    renderLinks(){
+    renderLinks(links){
         return links.map((link, index) => {
             return (
                 <li key={index}>
@@ -38,11 +33,22 @@ class Drawer extends Component {
             cls.push(classes.close)
         }
 
+        const links = [
+            { to: '/', label: 'Список', exact: true },
+        ]
+
+        if(this.props.isAuthenticated) {
+            links.push({ to: '/quiz-creator', label: 'Створити тест', exact: false })
+            links.push({ to: '/logout', label: 'Вийти', exact: false })
+        }else {
+            links.push({ to: '/auth', label: 'Авторизація', exact: false })
+        }
+
         return(
             <>
                 <nav className={cls.join(' ')}>
                     <ul>
-                        { this.renderLinks() }
+                        { this.renderLinks(links) }
                     </ul>
                 </nav>
                 {this.props.isOpen ? <Backdrop onClick={this.props.onClose}/> : null }
